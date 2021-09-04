4b95e079e17293e7c3fc8ad4eb97861905907ab0
ClaudioEC91 <corvalan.claudio47@gmail.com>
2021-09-04 18:03:41 -0300

    salvando las papas

--- src/manejoArchivos/Lector.java
+++ src/manejoArchivos/Lector.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import enums.TipoAtraccion;
 import parque.Usuario;
@@ -48,6 +50,37 @@ public class Lector {
 
 		return misUsuarios;
 
+	}
+	
+	
+public static Map<String, Atraccion> crearMapaAtraccion(String path) {
+		
+		Map<String, Atraccion> miMapaAtracciones = new HashMap<String, Atraccion>();
+		FileReader fr = null;
+		BufferedReader br = null;
+		
+		try {
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			
+			for (String linea = br.readLine(); linea != null; linea = br.readLine()) {
+				String[] aux =  linea.split(",");
+				//nombre costo tipo cupo y tiempoPromedio
+				Atraccion unaAtraccion = new Atraccion(aux[0], Double.parseDouble(aux[1]), TipoAtraccion.valueOf(aux[2]), Integer.parseInt(aux[3]),
+						Double.parseDouble(aux[4]));
+				miMapaAtracciones.put(aux[0], unaAtraccion);
+			}
+		} catch ( IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fr!=null)
+					fr.close();
+			} catch (Exception f) {
+				f.printStackTrace();
+			}
+		}
+		return miMapaAtracciones;
 	}
 	
 	
