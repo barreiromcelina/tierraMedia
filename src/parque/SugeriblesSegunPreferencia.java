e2adf28c04287cccdb6ca2ab00b84d615bdcba9b
Celina <barreiromcelina@gmail.com>
2021-09-04 11:29:35 -0300

    Refactor de comparator

--- src/parque/SugeriblesSegunPreferencia.java
+++ src/parque/SugeriblesSegunPreferencia.java
@@ -17,17 +17,38 @@ public class SugeriblesSegunPreferencia implements Comparator<Producto> {
 	public int compare(Producto o1, Producto o2) {
 		if (o1.getTipo() == this.tipoPreferido && o2.getTipo() == this.tipoPreferido) {
 			if (o1.esPromo() && o2.esPromo()) {
-				if (Double.compare(o1.getCosto(), o2.getCosto()) == 0) {
-					return -Double.compare(o1.getTiempoPromedio(), o2.getTiempoPromedio());
+				return comparadorCostoYtiempo(o1, o2);
 
-				} else {
-					return -Double.compare(o1.getCosto(), o2.getCosto());
-				}
-				
-			}//falta codigo
+			} else {
+				return -Boolean.compare(o1.esPromo(), o2.esPromo());
 
+			}
+
+		} else if (o1.getTipo() != this.tipoPreferido && o2.getTipo() != this.tipoPreferido) {
+			if (o1.esPromo() && o2.esPromo()) {
+				return comparadorCostoYtiempo(o1, o2);
+
+			} else if (!o1.esPromo() && !o2.esPromo()) {
+				return comparadorCostoYtiempo(o1, o2);
+
+			} else {
+				return -Boolean.compare(o1.esPromo(), o2.esPromo());
+			}
+
+		}else {
+			if(o1.getTipo()==this.tipoPreferido) return -1;
+			return 1;
 		}
-		return 0;
+		
+	}
+
+	private int comparadorCostoYtiempo(Producto o1, Producto o2) {
+		if (Double.compare(o1.getCosto(), o2.getCosto()) == 0) {
+			return -Double.compare(o1.getTiempoPromedio(), o2.getTiempoPromedio());
+
+		} else {
+			return -Double.compare(o1.getCosto(), o2.getCosto());
+		}
 	}
 
 }
