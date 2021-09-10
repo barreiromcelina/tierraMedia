5bab8db8b8baf6fa9c5faca3d97f50d896932491
ClaudioEC91 <corvalan.claudio47@gmail.com>
2021-09-10 14:02:16 -0300

    Comparador con metodos modularizados  para facil lectura del desarrollador + comentarios

--- src/parque/OrdenarSegunPreferencia.java
+++ src/parque/OrdenarSegunPreferencia.java
@@ -15,39 +15,42 @@ public class OrdenarSegunPreferencia implements Comparator<Producto> {
 
 	@Override
 	public int compare(Producto o1, Producto o2) {
+		return comparadorPreferencia(o1,o2);
+	}
+
+	private int comparadorPreferencia(Producto o1, Producto o2) {
+		//Si ambos son de su preferencia
 		if (o1.getTipo() == this.tipoPreferido && o2.getTipo() == this.tipoPreferido) {
-			if (o1.esPromo() && o2.esPromo()) {
-				return comparadorCostoYtiempo(o1, o2);
-
-			} else if (!o1.esPromo() && !o2.esPromo()) {
-				return comparadorCostoYtiempo(o1, o2);
+			//Voy a comparar si son promociones o atracciones sueltas
+			return comparadorProducto(o1,o2);
+		
+		//Si ambos no son de su preferencia
+		}else if (o1.getTipo() != this.tipoPreferido && o2.getTipo() != this.tipoPreferido) {
+			//Voy a comparar si son promociones o atracciones sueltas
+			return comparadorProducto(o1,o2);
 			
-			}else {
-				return -Boolean.compare(o1.esPromo(), o2.esPromo());
-
-			}
-
-		} else if (o1.getTipo() != this.tipoPreferido && o2.getTipo() != this.tipoPreferido) {
-			if (o1.esPromo() && o2.esPromo()) {
-				return comparadorCostoYtiempo(o1, o2);
-
-			} else if (!o1.esPromo() && !o2.esPromo()) {
-				return comparadorCostoYtiempo(o1, o2);
-
-			} else {
-				return -Boolean.compare(o1.esPromo(), o2.esPromo());
-			}
-
-		}else {
+		} else {
+			//Si un producto es preferido y el otro no. Ubico promero el preferido y despues el no preferido
 			if(o1.getTipo()==this.tipoPreferido) return -1;
 			return 1;
 		}
-		
 	}
-
 	
-	
-	
+	private int comparadorProducto(Producto o1, Producto o2) {
+		//Si ambas son promos
+		if (o1.esPromo() && o2.esPromo()) {
+			//voy a comparar costo y tiempo
+			return comparadorCostoYtiempo(o1, o2);
+			
+		//sino, miro si ambas son atracciones sueltas
+		} else if (!o1.esPromo() && !o2.esPromo()) {
+			//voy a comparar costo y tiempo
+			return comparadorCostoYtiempo(o1, o2);
+			
+		} else {//Sino ubico la promo primero y luego la atraccion
+			return -Boolean.compare(o1.esPromo(), o2.esPromo());
+		}
+	}
 	
 	private int comparadorCostoYtiempo(Producto o1, Producto o2) {
 		//Si los costos son iguales
