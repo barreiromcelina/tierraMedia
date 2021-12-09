ecd6b761e9fa9b46c00efaf773fcfefb169500f4
ClaudioEC91 <corvalan.claudio47@gmail.com>
2021-12-09 19:19:48 -0300

    modificado userService para que reciba todos los datos desde el servlet. tambien aca se encripta la contraseña del usuario

--- src/main/java/services/userService.java
+++ src/main/java/services/userService.java
@@ -1,14 +1,15 @@
 package services;
 
+import model.TipoAtraccion;
 import model.Usuario;
 import persistence.UsuarioDAO;
 import persistence.commons.DAOFactory;
 
 public class userService {
 
-	public Usuario create(String nombre, String contraseña) {
-		Usuario user = new Usuario(-1, false, nombre, contraseña, 0, 0, null);
-		
+	public Usuario create(String nombre, String contraseña, Double presupuesto, Double tiempoDisponible, TipoAtraccion tipoPreferido) {
+		Usuario user = new Usuario(-1, false, nombre, contraseña, presupuesto, tiempoDisponible, tipoPreferido);
+		user.setPassword(contraseña);
 		UsuarioDAO userDAO = DAOFactory.getUsuarioDAO();
 		userDAO.insert(user);
 		
